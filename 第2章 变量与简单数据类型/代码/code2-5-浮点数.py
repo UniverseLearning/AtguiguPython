a85863dfbdb6367d5288641e7a604e6bc0d09de9
MM <maowei@lrhealth.com>
2024-09-10 16:30:22 +0800

    1

--- 第2章 变量与简单数据类型/代码/code2-5-浮点数.py
+++ 第2章 变量与简单数据类型/代码/code2-5-浮点数.py
@@ -14,3 +14,38 @@ print("向上取整的结果是", n4)
 # 向下取整 floor
 n5 = math.floor(n1+n2)
 print("向下取整的结果是", n5)
+
+print(math.ceil(45.123123))
+print(math.floor(45.123123))
+
+print(True, False)
+
+if None:
+    print("None")
+else:
+    print("None111")
+
+if 0:
+    print("None")
+else:
+    print("None111")
+
+if 0.0:
+    print("None")
+else:
+    print("None111")
+
+if list():
+    print("None")
+else:
+    print("None111")
+
+if tuple():
+    print("None")
+else:
+    print("None111")
+
+if "":
+    print("None")
+else:
+    print("None111")
